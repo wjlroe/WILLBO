9bf7ba9cc6942d2054ae1ca2289dcc154e20365f
William Roe <git@wjlr.org.uk>
2019-05-19 12:58:49 +0100

    Changed some debug() calls to printf()
    We don't need file:lineno output on these.

--- minunit.h
+++ minunit.h
@@ -31,35 +31,44 @@
   mu_assert_changed(value, 0, test, message);
 
 #define mu_assert_str_equal(actual, expected)                                  \
-  mu_assert(strcmp(actual, expected) == 0, "'%s' does not equal '%s'", actual, \
+  mu_assert(strcmp(actual, expected) == 0,                                     \
+            "'%s' does not equal '%s'",                                        \
+            actual,                                                            \
             expected);
 
-#define mu_assert_array_equal(comparison_fn, actual, actual_len, expected,     \
-                              expected_len)                                    \
+#define mu_assert_array_equal(                                                 \
+  comparison_fn, actual, actual_len, expected, expected_len)                   \
   {                                                                            \
     mu_assert_int_equal((int)actual_len, (int)expected_len);                   \
     for (size_t i = 0; i < actual_len; i++) {                                  \
       mu_assert(comparison_fn(&actual[i], &expected[i]),                       \
-                "Element at index %lu not equal", i);                          \
+                "Element at index %lu not equal",                              \
+                i);                                                            \
     }                                                                          \
   }
 
 #define mu_assert_int_equal(actual, expected)                                  \
-  mu_assert(expected == actual, "%s (%i) does not equal %i", #actual, actual,  \
+  mu_assert(expected == actual,                                                \
+            "%s (%i) does not equal %i",                                       \
+            #actual,                                                           \
+            actual,                                                            \
             expected);
 
 #define mu_run_test(test)                                                      \
-  debug("\n-----%s", " " #test);                                               \
+  printf("\n-----%s\n", " " #test);                                            \
   test(test_run);                                                              \
   test_run->tests_run++;
 
 #define test_results(test_run)                                                 \
-  printf("%d tests run, %d assertions, %d failed\n", test_run->tests_run,      \
-         test_run->asserts_run, test_run->asserts_failed);
+  printf("%d tests run, %d assertions, %d failed\n",                           \
+         test_run->tests_run,                                                  \
+         test_run->asserts_run,                                                \
+         test_run->asserts_failed);
 
 #define check_test_args(num_args, arguments)
 
-typedef struct {
+typedef struct
+{
   int tests_run;
   int asserts_run;
   int asserts_passed;
@@ -67,18 +76,19 @@ typedef struct {
 } TestRun;
 
 #define RUN_TESTS(name)                                                        \
-  int main(int argc, char *argv[]) {                                           \
+  int main(int argc, char* argv[])                                             \
+  {                                                                            \
     argc = 1;                                                                  \
     if (argc > 1) {                                                            \
-      char *test_to_run = argv[1];                                             \
+      char* test_to_run = argv[1];                                             \
       printf("Run the %s test", test_to_run);                                  \
     }                                                                          \
-    TestRun *test_run = malloc(sizeof(TestRun));                               \
+    TestRun* test_run = malloc(sizeof(TestRun));                               \
     test_run->tests_run = 0;                                                   \
     test_run->asserts_run = 0;                                                 \
     test_run->asserts_passed = 0;                                              \
     test_run->asserts_failed = 0;                                              \
-    debug("----- RUNNING: %s", argv[0]);                                       \
+    printf("----- RUNNING: %s\n", argv[0]);                                    \
     name(test_run);                                                            \
     if (test_run->asserts_failed != 0) {                                       \
       printf(ANSI_COLOR_RED);                                                  \
